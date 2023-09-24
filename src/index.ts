8abc606fb582f5e4194b6fafd7e8db2f6af5e612
Gustavo Muñoz <timbergus@gmail.com>
2023-09-24 21:54:07 +0200

    Add title and feedback.

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,53 @@
+#! /usr/bin/env bun
+
 import { program } from 'commander'
 import { extractModel } from './tools/extractModel'
 import { extractValues } from './tools/extractValues'
-import { dirname } from 'node:path'
+// import { dirname } from 'node:path'
 import { existsSync, mkdirSync } from 'node:fs'
+import { textSync } from 'figlet'
+import chalk from 'chalk'
 
 program.name('sas-cli').description('CLI for SAS').version('0.0.1')
 
-program
-  .command('install')
-  .description('add the tools to manipulate models')
-  .argument('<path>', 'path to install the tools')
-  .action(async (path: string) => {
-    const file = Bun.file(`${path}/useModel.ts`)
-    if (await file.exists()) {
-      console.log('Already installed. Please, update.')
-    } else {
-      Bun.spawn(['cp', '-R', 'src/hooks', path])
-    }
-  })
+// TODO Make hooks install option.
+// program
+//   .command('install')
+//   .description('add the tools to manipulate models')
+//   .argument('<path>', 'path to install the tools')
+//   .action(async (path: string) => {
+//     const file = Bun.file(`${path}/useModel.ts`)
+//     if (await file.exists()) {
+//       console.log('Already installed. Please, update.')
+//     } else {
+//       Bun.spawn(['cp', '-R', 'src/hooks', path])
+//     }
+//   })
 
-program
-  .command('update')
-  .description('updates the tools to manipulate models')
-  .action(async () => {
-    const { stdout } = Bun.spawn([
-      'find',
-      '.',
-      '-iname',
-      'useModel.ts',
-      '!',
-      '-path',
-      '*/src/*',
-    ])
-    const stdoutStr = await new Response(stdout).text()
-    if (stdoutStr) {
-      const path = dirname(stdoutStr)
-      console.log('🚀 ~ file: index.ts:38 ~ .action ~ path:', path)
-      Bun.spawn(['rm', '-rf', path])
-      Bun.spawn(['cp', '-R', 'src/hooks', 'pepe'])
-    } else {
-      console.log('Not installed. Please, install.')
-    }
-  })
+// TODO Make hooks update option.
+// program
+//   .command('update')
+//   .description('updates the tools to manipulate models')
+//   .action(async () => {
+//     const { stdout } = Bun.spawn([
+//       'find',
+//       '.',
+//       '-iname',
+//       'useModel.ts',
+//       '!',
+//       '-path',
+//       '*/src/*',
+//     ])
+//     const stdoutStr = await new Response(stdout).text()
+//     if (stdoutStr) {
+//       const path = dirname(stdoutStr)
+//       console.log('🚀 ~ file: index.ts:38 ~ .action ~ path:', path)
+//       Bun.spawn(['rm', '-rf', path])
+//       Bun.spawn(['cp', '-R', 'src/hooks', 'pepe'])
+//     } else {
+//       console.log('Not installed. Please, install.')
+//     }
+//   })
 
 program
   .command('extract')
@@ -54,11 +60,17 @@ program
     }
     if (file.toLowerCase().includes('model.xlsx')) {
       extractModel(file, options.path)
+      console.log(chalk.cyan('✅', 'Model extracted successfully', '\n'))
     } else if (file.toLowerCase().includes('values.xlsx')) {
       extractValues(file, options.path)
+      console.log(chalk.cyan('✅', 'Values extracted successfully', '\n'))
     } else {
-      console.log('Please select a model or values XLSX file')
+      console.log(
+        chalk.red('❗️', 'Please select a model or values XLSX file', '\n')
+      )
     }
   })
+
+console.log(chalk.blue(textSync('SAS CLI')))
 
 program.parse()
